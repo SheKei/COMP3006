6f4a14e96b175a645da3ba3cbbfa26036c1451e8
SheKei <43881082+SheKei@users.noreply.github.com>
2021-07-28 07:26:27 +0100

    Book Model

--- Database.js
+++ Database.js
@@ -5,4 +5,7 @@ let dbUrl = "mongodb://localhost:27017/butterflydb";
 mongoose.connect(dbUrl, {useUnifiedTopology: true, useNewUrlParser: true});
 
 //IMPORT SCHEMAS
+let Account = require("./Schema/Account-Schema").Account;
+let Book = require("./Schema/Book-Schema").Book;
 
+//IMPORT MODEL CLASSES
---
+++ Model/Book.js
@@ -0,0 +1,46 @@
+class Book{
+
+    constructor(authorForename, authorSurname, bookName, stockPrice, sellingPrice, stockAmount, synopsis, genres) {
+
+        this._authorForename = authorForename;
+        this._authorSurname = authorSurname;
+        this._bookName = bookName;
+        this._stockPrice = stockPrice;
+        this._sellingPrice = sellingPrice;
+        this._stockAmount = stockAmount;
+        this._synopsis = synopsis;
+        this._genres = genres;
+    }
+    
+    get authorForename() {
+        return this._authorForename;
+    }
+
+    get authorSurname() {
+        return this._authorSurname;
+    }
+
+    get bookName() {
+        return this._bookName;
+    }
+
+    get stockPrice() {
+        return this._stockPrice;
+    }
+
+    get sellingPrice() {
+        return this._sellingPrice;
+    }
+
+    get stockAmount() {
+        return this._stockAmount;
+    }
+
+    get synopsis() {
+        return this._synopsis;
+    }
+
+    get genres() {
+        return this._genres;
+    }
+}
